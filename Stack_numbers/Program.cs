eed61d92b8e822a79b1192774692eb0a76e9e621
Lacika <vargalacii111@gmail.com>
2022-10-20 15:01:12 +0200

    Add project files.

---
+++ Stack_numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+internal static class NumberProgram
+{
+    public static void Main(string[] args)
+    {
+
+        Stack stack = new Stack();
+        int szam;
+
+        Console.WriteLine("Add meg az első számot!");
+        szam = Convert.ToInt32(Console.ReadLine());
+        while (szam != 0)
+        {
+            stack.Push(szam);
+            Console.WriteLine("Add meg a következő számot!");
+            szam = Convert.ToInt32(Console.ReadLine());
+
+        }
+        Console.WriteLine("\nA számok visszafelé: ");
+        foreach (var szamok in stack)
+        {
+            
+            Console.WriteLine(szamok);
+        }
+    }
+}
+
